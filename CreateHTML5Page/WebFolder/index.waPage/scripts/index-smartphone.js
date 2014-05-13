5b1a5cd61c217f79952bffe41038e4a816048aa2
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-05-13 12:39:45 -0500

    Update #5

--- CreateHTML5Page/WebFolder/index.waPage/scripts/index-smartphone.js
+++ CreateHTML5Page/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,11 +2,38 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
 	var Disconnect = {};	// @button
 	var Connect = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		var options = new ContactFindOptions(), name, phoneNumber;
+		options.filter = "";
+		options.multiple = true;
+		 
+		var fields = ["name", "phoneNumbers"];
+		 
+		function onSuccess(contacts) {
+		var res = "";
+		for(var index = 0, len = contacts.length; index < len; index++) { name = contacts[index].name; name = name != null ? name.formatted : ""; phoneNumber = contacts[index].phoneNumbers; phoneNumber = phoneNumber != null && phoneNumber.length > 0 ? phoneNumber[0].value : "";
+		 
+		res += name + "   : " + phoneNumber + "n";
+		}
+		$$('textField2').setValue(res);
+		 
+		navView.goToView(3);
+		}
+		 
+		function onError() {
+		alert('onError!');
+		}
+		 
+		navigator.contacts.find(fields, onSuccess, onError, options);
+	};// @lock
 
 	Disconnect.click = function Disconnect_click (event)// @startlock
 	{// @endlock
@@ -19,6 +46,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
 	WAF.addListener("Disconnect", "click", Disconnect.click, "WAF");
 	WAF.addListener("Connect", "click", Connect.click, "WAF");
 // @endregion
