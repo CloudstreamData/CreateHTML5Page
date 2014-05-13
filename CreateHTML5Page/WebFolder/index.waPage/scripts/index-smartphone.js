d37858923576b826eed3c13eca7abd425c08efc5
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-05-13 11:54:48 -0500

    Update #3

---
+++ CreateHTML5Page/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -0,0 +1,25 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var Disconnect = {};	// @button
+	var Connect = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	Disconnect.click = function Disconnect_click (event)// @startlock
+	{// @endlock
+		$$("navigationView3").goToNextView();
+	};// @lock
+
+	Connect.click = function Connect_click (event)// @startlock
+	{// @endlock
+		$$("navigationView2").goToNextView();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("Disconnect", "click", Disconnect.click, "WAF");
+	WAF.addListener("Connect", "click", Connect.click, "WAF");
+// @endregion
+};// @endlock
